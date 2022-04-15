d76558189a0c29cf98030c9153b30668f2faa8af
Fellipe Neves <fellipe_emanoel@univem.edu.br>
2022-04-15 11:02:43 -0300

    Created application theme with styled components

---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    primary: "#130160",
+    secondary: "#FF9228",
+    tertiary: "#D6CDFE",
+    background: "#E5E5E5",
+    background_secondary: "#F9F9F9",
+    background_tertiary: "#F3F2F2",
+    fill: "#FFFFFF",
+    title: "#0D0140",
+    text: "#524B6B",
+    item_menu_primary: "#AFECFE",
+    item_menu_secondary: "#BEAFFE",
+    item_menu_tertiary: "#FFD6AD",
+  },
+  fonts:{
+      primary_700: 'Inter_400Regular',
+      primary_400: 'Inter_500Medium'
+  }
+}
